82fbe6b5cd2405eaaafd859b94a10d65ce239d28
J4SON <jason@chatr.fr>
2022-07-30 11:43:59 +0200

    update

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = ({}) => {
 						<button
 							onClick={() => setIsOpen(!isOpen)}
 							type="button"
-							className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-200 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-200 transition duration-150 ease-in-out"
+							className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-red-400 hover:bg-transparent focus:outline-none focus:bg-transparent focus:text-red-400 transition duration-150 ease-in-out"
 						>
 							{isOpen ? (
 								<svg
@@ -64,7 +64,7 @@ const Navbar = ({}) => {
 							</a>
 						</Link>
 						<span className="text-white text-sm hidden md:block">
-							{('Fire')}
+							{''}
 						</span>
 					</div>
 					{/* End logo */}
@@ -75,32 +75,32 @@ const Navbar = ({}) => {
 								className="router-link-active router-link-exact-active border-main-color text-red-400 inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2"
 								aria-current="page"
 							>
-								{('Home')}
+								{'Home'}
 							</a>
 						</Link>
-						<Link href="/Quizplay">
-							<a className="border-transparent text-gray-200 hover:border-gray-300 hover:text-red-500 inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2">
-								{('Quiz Play')}
+						<Link href="/Quiz_play">
+							<a className="border-transparent text-gray-200 hover:border-red-400 hover:text-red-400 inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2">
+								{'Quiz Play'}
 							</a>
 						</Link>
 						<Link href="/Bookmark">
-							<a className="border-transparent text-gray-200 hover:border-gray-300 hover:text-red-500 inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2">
-								{('Bookmark')}
+							<a className="border-transparent text-gray-200 hover:border-red-400 hover:text-red-400 inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2">
+								{'Bookmark'}
 							</a>
 						</Link>
 						<Link href="/Invite_friends">
-							<a className="border-transparent text-gray-200 hover:border-gray-300 hover:text-red-500 inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2">
-								{('Invite Friends')}
+							<a className="border-transparent text-gray-200 hover:border-red-400 hover:text-red-400 inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2">
+								{'Invite Friends'}
 							</a>
 						</Link>
 						<Link href="/Instruction">
-							<a className="border-transparent text-gray-200 hover:border-gray-300 hover:text-red-500 inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2">
-								{('Instruction')}
+							<a className="border-transparent text-gray-200 hover:border-red-400 hover:text-red-400 inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2">
+								{'Instruction'}
 							</a>
 						</Link>
-						<Link href="/Leaderboard">
-							<a className="border-transparent text-gray-200 hover:border-gray-300 hover:text-red-500 inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2">
-								{('Leaderboard')}
+						<Link href="/Leader_board">
+							<a className="border-transparent text-gray-200 hover:border-red-400 hover:text-red-400 inline-flex items-center px-1 pt-1 text-sm font-medium border-b-2">
+								{'Leader board'}
 							</a>
 						</Link>
 					</div>
@@ -125,7 +125,7 @@ const Navbar = ({}) => {
 								type="button"
 								className="hidden text-sm text-white md:inline-flex focus:outline-none text-second-color-500 hover:text-second-color-900"
 							>
-								{('Sign In')}
+								{'Sign In'}
 							</a>
 						</Link>
 						<Link href="/">
@@ -133,9 +133,67 @@ const Navbar = ({}) => {
 								type="button"
 								className="hidden px-4 py-2 text-sm text-white rounded-md border border-transparent shadow-sm md:inline-flex bg-red-400 hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-main-color-500"
 							>
-								{('Sign Up')}
+								{'Sign Up'}
 							</a>
 						</Link>
+					</div>
+				</div>
+			</div>
+			{/* Mobile nav */}
+			<div className={`${isOpen ? 'block' : 'hidden'} md:hidden`}>
+				<div className="pt-2 pb-3 space-y-1">
+					<Link href="/">
+						<a className="router-link-active router-link-exact-active border-red-500 text-red-400 text-base font-medium sm:pl-5 sm:pr-6 block pl-3 pr-4 py-2 border-l-4">
+							{'Home'}
+						</a>
+					</Link>
+					<Link href="/Quiz_play">
+						<a className="border-transparent text-gray-200 hover:bg-gray-50 hover:border-red-400 hover:text-gray-700 text-base font-medium sm:pl-5 sm:pr-6 block pl-3 pr-4 py-2 border-l-4">
+							{'Quiz play'}
+						</a>
+					</Link>
+					<Link href="/Bookmark">
+						<a className="border-transparent text-gray-200 hover:bg-gray-50 hover:border-red-400 hover:text-gray-700 text-base font-medium sm:pl-5 sm:pr-6 block pl-3 pr-4 py-2 border-l-4">
+							{'Bookmark'}
+						</a>
+					</Link>
+					<Link href="/Invite_friends">
+						<a className="border-transparent text-gray-200 hover:bg-gray-50 hover:border-red-400 hover:text-gray-700 text-base font-medium sm:pl-5 sm:pr-6 block pl-3 pr-4 py-2 border-l-4">
+							{'Invite friends'}
+						</a>
+					</Link>
+					<Link href="/Instruction">
+						<a className="border-transparent text-gray-200 hover:bg-gray-50 hover:border-red-400 hover:text-gray-700 text-base font-medium sm:pl-5 sm:pr-6 block pl-3 pr-4 py-2 border-l-4">
+							{'Instruction'}
+						</a>
+					</Link>
+					<Link href="/Leader_board">
+						<a className="border-transparent text-gray-200 hover:bg-gray-50 hover:border-red-400 hover:text-gray-700 text-base font-medium sm:pl-5 sm:pr-6 block pl-3 pr-4 py-2 border-l-4">
+							{'Leader board'}
+						</a>
+					</Link>
+				</div>
+				<div className="pt-4 pb-3 border-t border-gray-200">
+					<div className="flex items-center px-4 sm:px-6">
+						<div className="flex items-center space-x-4">
+							<Link href="/">
+								<a className="px-4 py-2 text-sm text-white rounded-md border border-transparent shadow-sm bg-red-400 hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-main-color-500">
+									{'Sign Up'}
+								</a>
+							</Link>
+							<Link href="/">
+								<a className="text-sm text-white focus:outline-none text-second-color-500 hover:text-second-color-900">
+									{'Sign In'}
+								</a>
+							</Link>
+						</div>
+						<div className="relative ml-auto">
+							<div className="">
+								<button className="text-sm text-white focus:outline-none text-second-color-500 hover:text-second-color-900">
+									<span>English</span>
+								</button>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -143,4 +201,3 @@ const Navbar = ({}) => {
 	);
 };
 export default withTranslation()(Navbar);
-
